1260e14de7bb501e326d32f683f469335ef10513
Onekki <zhuwangqi@foxmail.com>
2022-08-18 16:40:52 +0800

    Add ok-list

--- content/index.js
+++ content/index.js
@@ -1,5 +1,6 @@
 const divs = {}
 $(document).ready(() => {
+    $('body').append('<div class="ok-list ok-card ok-float"></div>')
     window.addEventListener("mousedown", (e) => {
         if (e && e.altKey) {
             e.preventDefault()
@@ -53,7 +54,7 @@ function pop(request) {
     }
     isRunning = true
     const target = divs[request.id]
-    const container = $('<div class="ok-container ok-card"><div class="ok-left"><img width="100%" height="100%" class="ok-img" /></div><div class="ok-space"></div><div class="ok-right"><div class="ok-status"></div><a class="ok-open">打开目录</a></div></div>')
+    const container = $('<div class="ok-container ok-card ok-float"><div class="ok-left"><img width="100%" height="100%" class="ok-img" /></div><div class="ok-space"></div><div class="ok-right"><div class="ok-status"></div><a class="ok-open">打开目录</a></div></div>')
     if (target && target.currentSrc) container.find('.ok-img').attr('src', target.currentSrc)
     container.find('.ok-open').click((e) => {
         console.log('ok-open', e)
@@ -62,37 +63,38 @@ function pop(request) {
         })
     })
     container.addClass('.' + request.id)
+    if (request.success) {
+        container.find('.ok-status').text('保存成功')
+        container.find('.ok-status').css('color', '#06ae56')
+    } else {
+        container.find('.ok-status').text('保存失败')
+        container.find('.ok-status').css('color', '#fa5151')
+        container.find('.ok-img').css('background-color', '#aaa')
+        container.find('.ok-img').css('border-radius', '8px')
+    }
     $('body').append(container)
 
+    anime({
+        targets: $('.' + request.id),
+        translateX: 48,
+        duration: 500,
+        opacity: 0,
+        easing: 'easeInOutQuad'
+    })
+
     const complete = () => {
-        console.log('append')
-        container.css('opacity', 1)
-
-        if (request.success) {
-            $('.ok-status').text('保存成功')
-            $('.ok-status').css('color', '#06ae56')
-        } else {
-            $('.ok-status').text('保存失败')
-            $('.ok-status').css('color', '#fa5151')
-        }
-
-        setTimeout(() => {
-            console.log('remove')
-            container.remove()
-        }, 3000)
+        setTimeout(() => container.remove(), 3000)
 
         isRunning = false
         popQueue.pop()
         divs[request.id] = null
-        if (popQueue.length > 0) {
-            pop(popQueue[0])
-        }
+        if (popQueue.length > 0) pop(popQueue[0])
     }
 
     if (target) {
         const imgOffset = $('.ok-img').offset()
         const targetOffset = $(target).offset()
-        const a = anime({
+        const aAnime = anime({
             targets: [target],
             scale: 48 / target.width,
             translateX: imgOffset.left - targetOffset.left,
@@ -102,7 +104,7 @@ function pop(request) {
             easing: 'easeInOutQuad',
             complete: () => {
                 complete()
-                a.reset()
+                aAnime.reset()
                 $(target).css('opacity', 0.5)
             }
         })
